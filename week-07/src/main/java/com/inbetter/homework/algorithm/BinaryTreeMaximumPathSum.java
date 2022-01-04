e0c12ff374a33f84cadcc6151641b6855a4ab5cf
imxianjun <imxianjun@gmail.com>
2022-01-04 13:17:40 +0800

    add BinaryTreeMaximumPathSum.

--- week-07/src/main/java/com/inbetter/homework/algorithm/BinaryTreeMaximumPathSum.java
+++ week-07/src/main/java/com/inbetter/homework/algorithm/BinaryTreeMaximumPathSum.java
@@ -2,10 +2,30 @@ package com.inbetter.homework.algorithm;
 
 public class BinaryTreeMaximumPathSum {
 
-    public int maxPathSum(TreeNode root) {
+    private int maxPathSum;
 
+    public int maxPathSum(TreeNode root) {
+        maxPathSum = Integer.MIN_VALUE;
+        maxSumHelper(root);
+        return maxPathSum;
     }
-    
+
+    private int maxSumHelper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftSum = maxSumHelper(root.left);
+        int rightSum = maxSumHelper(root.right);
+
+        int maxSumInLeftAndRight = Math.max(leftSum, rightSum);
+        int maxSumInSinglePart = Math.max(root.val, root.val + maxSumInLeftAndRight);
+        int maxSumAll = Math.max(maxSumInSinglePart, leftSum + rightSum + root.val);
+
+        maxPathSum = Math.max(maxPathSum, maxSumAll);
+
+        return maxSumInSinglePart;
+    }
+
     public class TreeNode {
         int val;
         TreeNode left;
