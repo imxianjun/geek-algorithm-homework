927f4135f1a6e8dedcc23b48da0e33ab9dbd975f
imxianjun <imxianjun@gmail.com>
2022-02-11 11:51:30 +0800

    add code(48) in geek-time , the question is very difficult but i cannot give up.

--- leetcode-algorithm/src/main/java/com/inbetter/homework/leetcode/BestTimeToBuyAndSellStockWithCooldown.java
+++ leetcode-algorithm/src/main/java/com/inbetter/homework/leetcode/BestTimeToBuyAndSellStockWithCooldown.java
@@ -4,22 +4,29 @@ public class BestTimeToBuyAndSellStockWithCooldown {
 
     public int maxProfit(int[] prices) {
         int length = prices.length;
-        int[][][] maxProfits = new int[length + 1][length + 1][2];
-        for (int i = 1; i <= length; i++) {
-            for (int j = length - 1; j >= 0; j--) {
-                for (int l = 0; l < 2; l++) {
-                    int doNoting = maxProfits[j + 1][i][l];
-                    int doSomething;
-                    if (l == 0) {
-                        doSomething = maxProfits[j + 1][i][1] - prices[j];
-                    } else {
-                        doSomething = maxProfits[j + 1][i - 1][0] + prices[j];
-                    }
-                    maxProfits[j][i][l] = Math.max(doNoting, doSomething);
-                }
-            }
+        int[][] maxProfits = new int[length + 1][3];
+        maxProfits[0][1] = -prices[0];
+        int ans = 0;
+        for (int i = 1; i < prices.length; i++) {
+            maxProfits[i][0] = Math.max(maxProfits[i - 1][0], maxProfits[i - 1][2]);
+            maxProfits[i][1] = Math.max(maxProfits[i - 1][1], maxProfits[i - 1][0] - prices[i]);
+            maxProfits[i][2] = maxProfits[i - 1][1] + prices[i];
+            ans = Math.max(Math.max(maxProfits[i][0], maxProfits[i][1]), maxProfits[i][2]);
         }
-        return maxProfits[0][length][0];
+
+        return ans;
+    }
+
+    public static void print(int[][] maxProfits){
+        for (int i = 0; i < maxProfits.length; i++)
+            for (int j = 0; j < maxProfits[0].length; j++)
+                System.out.println("arr[" + i + "][" + j + "] = "
+                        + maxProfits[i][j]);
+    }
+
+    public static void main(String[] args) {
+        int[] prices = new int[]{1, 2, 3, 0, 2};
+        new BestTimeToBuyAndSellStockWithCooldown().maxProfit(prices);
     }
 
 }
--- leetcode-algorithm/src/main/java/com/inbetter/homework/leetcode/BestTimeToBuyAndSellStockWithTransactionFee.java
+++ leetcode-algorithm/src/main/java/com/inbetter/homework/leetcode/BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -1,4 +1,23 @@
 package com.inbetter.homework.leetcode;
 
 public class BestTimeToBuyAndSellStockWithTransactionFee {
+
+    public int maxProfit(int[] prices, int fee) {
+        int length = prices.length;
+        int[][] maxProfits = new int[length + 1][2];
+        for (int j = length - 1; j >= 0; j--) {
+            for (int l = 0; l < 2; l++) {
+                int doNoting = maxProfits[j + 1][l];
+                int doSomething;
+                if (l == 0) {
+                    doSomething = maxProfits[j + 1][1] - prices[j];
+                } else {
+                    doSomething = maxProfits[j + 1][0] + prices[j] - fee;
+                }
+                maxProfits[j][l] = Math.max(doNoting, doSomething);
+            }
+        }
+        return maxProfits[0][0];
+    }
+
 }
