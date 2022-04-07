4bd197b7559de3926d73d3987379413cc5a5edc7
imxianjun <imxianjun@gmail.com>
2022-04-07 19:01:10 +0800

    add MergeSortedArray , MoveZeroes , RemoveDuplicatesFromSortedArray .

--- week-01/src/main/java/com/inbetter/homework/algorithm/MoveZeroes.java
+++ week-01/src/main/java/com/inbetter/homework/algorithm/MoveZeroes.java
@@ -1,4 +1,18 @@
 package com.inbetter.homework.algorithm;
 
 public class MoveZeroes {
+
+    public void moveZeroes(int[] nums) {
+        int n = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[n++] = nums[i];
+            }
+        }
+        while (n < nums.length) {
+            nums[n] = 0;
+            n++;
+        }
+    }
+
 }
--- week-01/src/main/java/com/inbetter/homework/algorithm/RemoveDuplicatesFromSortedArray.java
+++ week-01/src/main/java/com/inbetter/homework/algorithm/RemoveDuplicatesFromSortedArray.java
@@ -1,4 +1,16 @@
 package com.inbetter.homework.algorithm;
 
 public class RemoveDuplicatesFromSortedArray {
+
+    public int removeDuplicates(int[] nums) {
+        int n = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                nums[n] = nums[i];
+                n++;
+            }
+        }
+        return n;
+    }
+
 }
