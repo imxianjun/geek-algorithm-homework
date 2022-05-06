373692c316faa93082d23a8fdfbbd2c6513d627d
imxianjun <imxianjun@gmail.com>
2022-05-06 08:54:11 +0800

    add ReverseLinkedList , ReverseNodesInKGroup .

--- week-01/src/main/java/com/inbetter/homework/algorithm/ReverseLinkedList.java
+++ week-01/src/main/java/com/inbetter/homework/algorithm/ReverseLinkedList.java
@@ -1,4 +1,33 @@
 package com.inbetter.homework.algorithm;
 
 public class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode last = null;
+        while (head != null) {
+            ListNode nextHead = head.next;
+            head.next = last;
+            last = head;
+            head = nextHead;
+        }
+        return last;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
 }
--- week-01/src/main/java/com/inbetter/homework/algorithm/ReverseNodesInKGroup.java
+++ week-01/src/main/java/com/inbetter/homework/algorithm/ReverseNodesInKGroup.java
@@ -1,4 +1,64 @@
 package com.inbetter.homework.algorithm;
 
 public class ReverseNodesInKGroup {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode protect = new ListNode(0, head);
+        ListNode last = protect;
+        while (head != null) {
+            ListNode end = getEnd(head, k);
+            if (end == null) {
+                break;
+            }
+            ListNode nextGroupHead = end.next;
+            reverseList(head, nextGroupHead);
+
+            last.next = end;
+            head.next = nextGroupHead;
+
+            last = head;
+            head = nextGroupHead;
+        }
+        return protect.next;
+    }
+
+    private void reverseList(ListNode head, ListNode stop) {
+        ListNode last = head;
+        head = head.next;
+        while (head != stop) {
+            ListNode nextHead = head.next;
+            head.next = last;
+            last = head;
+            head = nextHead;
+        }
+    }
+
+    private ListNode getEnd(ListNode head, int k) {
+        while (head != null) {
+            k--;
+            if (k == 0) {
+                return head;
+            }
+            head = head.next;
+        }
+        return null;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
 }
